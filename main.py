0e41dcd230625af21f9106d5bee849a604b4c694
Héctor de Anda <30514545+Hectorreto@users.noreply.github.com>
2024-11-08 18:29:14 -0600

    feat: record any mouse button pressed released and held down

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pynput import mouse
 option = ''
 start_time = 0
 record = []
+all_buttons = {}
 
 def repeat_record():
     if len(record) == 0:
@@ -15,7 +16,9 @@ def repeat_record():
 
     while option == '3':
         if step < len(record):
-            x, y, t = record[step]
+            x, y, button, pressed, t = record[step]
+            action = 'Pressed' if pressed else 'Released'
+
             if sub_step == 0:
                 time.sleep((t - last_time) * 2/3)
                 sub_step = 1
@@ -23,11 +26,15 @@ def repeat_record():
                 pyautogui.moveTo(x, y, duration=(t - last_time) * 1/3)
                 sub_step = 2
             else:
-                pyautogui.click(x, y)
+                if pressed:
+                    pyautogui.mouseDown(x, y, button=button.name)
+                else:
+                    pyautogui.mouseUp(x, y, button=button.name)
+                    
                 step += 1
                 last_time = t
                 sub_step = 0
-                print(f'Clicked at {x}, {y} in {t}')
+                print(f'{action} at {x}, {y} in {t}')
         else:
             step = 0
             last_time = 0
@@ -55,7 +62,6 @@ def keyboard_listener():
         if key == '1' and option != '1':
             print("Recording...")
             option = '1'
-            start_time = time.time()
             record = []
 
         if key == '2' and option != '2':
@@ -73,13 +79,26 @@ def keyboard_listener():
             return
 
 def on_click(x, y, button, pressed):
-    if option != '1' or not pressed or not button == mouse.Button.left:
+    if option != '1':
         return
+    
+    global start_time
+    action = 'pressed' if pressed else 'released'
 
-    print(f'Clicked at {x}, {y} in {time.time() - start_time}')
-    record.append((x, y, time.time() - start_time))
+    if len(record) == 0:
+        start_time = time.time()
+
+    all_buttons[button.name] = action
+        
+    print(f'{button.name} {action} at {x}, {y} in {time.time() - start_time}')
+    record.append((x, y, button, pressed, time.time() - start_time))
 
 
 threading.Thread(target=keyboard_listener).start()
 mouse.Listener(on_click=on_click).start()
 show_instructions()
+
+# release all mouse buttons
+for button in all_buttons:
+    if all_buttons[button] == 'pressed':
+        pyautogui.mouseUp(button=button)
